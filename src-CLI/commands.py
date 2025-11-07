0b1f7798134ca6d053f133356911da3e40deff5b
JavasciptBeast <142036809+JavasciptBeast@users.noreply.github.com>
2025-11-07 07:55:26 -0600

    Refactor linear equation in commands.py

--- src-CLI/commands.py
+++ src-CLI/commands.py
@@ -29,18 +29,18 @@ def multiply_prompt():
 def lineareq():
     """solves basic linears
     """
-    eq = input("enter equation,format in x + b = c\nex x + 1 = 9\n leave a space before please")
+    eq = input("enter equation,format in x + b = c\nex x + 1 = 9\n")
+    print(eq)
     length = -1
     for letter in eq:
         length+=1
         match letter:
             case "x":
-                if eq[-(length-1)] != "":
-                    coeff=eq[-(length-1)]
-                    print(f"{coeff}")
-                else:
-                    print("no coeff")
+                
+                
+                coeff=eq.split("x")[0]
+                print(f"COEF {coeff}")
+                
             case "=":
                 after = eq[length+1]
                 print(f"{after}")
-                
