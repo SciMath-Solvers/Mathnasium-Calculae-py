6c53725375d5eee841321de505a4b9a2b1fc716c
JavasciptBeast <142036809+JavasciptBeast@users.noreply.github.com>
2025-11-09 12:52:49 -0600

    Implement linear equation solver function
    Added a new function to solve basic linear equations.

--- src-CLI/funcs.py
+++ src-CLI/funcs.py
@@ -26,8 +26,26 @@ def multiply_prompt():
     a = int(input(">_< *_?\n"))
     b = int(input(f"{a} * >_<?\n"))
     print(f"= {a / b}")
-        
+
+def lineareq():
+    """solves basic linears
+    """
+    eq = input("enter equation,format in x + b = c\nex x + 1 = 9\n")
+    print(eq)
+    length = -1
+    for letter in eq:
+        length+=1
+        match letter:
+            case "x":
+                eq.replace(" ","")
+                coeff=eq.split("x")[0]
+                print(f"COEF {coeff}")
+                after_x=eq.split("x")[1]
+                after = after_x.split("=")[1]
+                
+                      
 def exp():
+    """doesnt work"""
     exp = None
     while exp == None:
         exp = input("Enter All-in-One Expression: ") | ""
@@ -44,3 +62,4 @@ def Choice()->int:
     opt = input("1.Add \n2.Divide \n3.Subtract \n4.Multiply \n5.Discontunied \n6. All-in-One (Includes CAS) \n7. Exit \n8. About")
 
     return int(opt)
+
